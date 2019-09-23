b2adbae1051fc3e42db47dc3cb7bd416de568584
HLXEasy <hlxeasy@gmail.com>
2019-09-23 23:41:46 +0200

    First steps towards chain data cleanup

--- windows/packages/Blockchain/meta/installscript.js
+++ windows/packages/Blockchain/meta/installscript.js
@@ -35,6 +35,10 @@ Component.prototype.createOperationsForArchive = function(archive)
     // don't use the default operation
     // component.createOperationsForArchive(archive);
 
+    // Cleanup existing blockchain data
+    component.addOperation("Delete", "@HomeDir@/AppData/Roaming/InstallerTest/subfolder")
+    component.addOperation("Delete", "@HomeDir@/AppData/Roaming/InstallerTest/.gitignore")
     // add an extract operation with a modified path
-    component.addOperation("Extract", archive, "@TargetDir@/extractToAnotherPath");
+    // component.addOperation("Extract", archive, "@TargetDir@/extractToAnotherPath");
+    component.addOperation("Extract", archive, "@HomeDir@/AppData/Roaming/InstallerTest");
 }
