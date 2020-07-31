580ed4adbb55030c0f439f571a0e84cf2a031641
Helix <hlxeasy@gmail.com>
2020-08-01 00:05:54 +0200

    Implemented first version of Spectrecoin2Alias migration script

--- linux/migrateSpectrecoinToAliaswallet.sh
+++ linux/migrateSpectrecoinToAliaswallet.sh
@@ -16,6 +16,7 @@
 # ============================================================================
 
 versionToInstall=$1
+installPath=/usr/local/bin
 
 # ----------------------------------------------------------------------------
 # Create backup of wallet.dat
@@ -33,17 +34,18 @@ if [[ -e ~/.spectrecoin/wallet.dat ]] ; then
 fi
 
 # ----------------------------------------------------------------------------
-# Check disk space and and if at least the same amount +10% used by the data
-# dir is available, copy data directory from ~/.spectrecoin to ~/.aliaswallet
+# Check disk space and and if at least the same amount as the data dir +10%
+# is available, copy data directory from ~/.spectrecoin to ~/.aliaswallet.
+# Otherwise ask if the folder should be renamed.
 if [[ -d ~/.spectrecoin/ ]] ; then
     usedDiskSpace=$(du -s ~/.spectrecoin | awk '{print $1}')
-    usedDiskSpaceWithBuffer=$(echo "${usedDiskSpace} * 1.1" | bc -l)
+    usedDiskSpaceWithBuffer=$(echo "scale = 0; ${usedDiskSpace} * 1.1 / 1" | bc -l)
     freeSpaceOnHomeDir=$(df ~ | tail -n 1 | awk '{print $4}')
-    printf "Free disk space:                                      %15s\n" ${freeSpaceOnHomeDir}
-    printf "Used disk space for spectrecoin data:                 %15s\n" ${usedDiskSpace}
-    printf "Used disk space for spectrecoin data with 10%% buffer: %15s\n" ${usedDiskSpaceWithBuffer}
+    printf "Available disk space:                             %15s\n" ${freeSpaceOnHomeDir}
+    printf "Used disk space for spectrecoin data:             %15s\n" ${usedDiskSpace}
+    printf "Used disk space for spectrecoin data +10%% buffer: %15s\n" ${usedDiskSpaceWithBuffer}
     if [[ ${freeSpaceOnHomeDir} -gt ${usedDiskSpaceWithBuffer} ]] ; then
-        echo "Renaming data directory ~/.spectrecoin to ~/.aliaswallet"
+        echo "Copying data directory ~/.spectrecoin to ~/.aliaswallet"
         echo "Patience please..."
         cp -r ~/.spectrecoin ~/.aliaswallet
         echo "    Done"
@@ -51,22 +53,89 @@ if [[ -d ~/.spectrecoin/ ]] ; then
         echo
         echo "Not enough disk space to duplicate Spectrecoin data directory!"
         echo
-        echo -n "Rename it instead of creating a copy [y/n]? "
+        echo "Rename it instead of creating a copy [y/n]?"
+        echo -n "Everything else than 'y' will cancel the update: "
         read input
-        if [[ "${input}" = 'y' ]] ; then
+        if [[ -n "${input}" && "${input}" = 'y' ]] ; then
             mv ~/.spectrecoin ~/.aliaswallet
         else
+            echo "Update canceled"
             exit 1
         fi
         echo "    Done"
     fi
 fi
+
+# ----------------------------------------------------------------------------
+# Rename configuration file
 if [[ -e ~/.aliaswallet/spectrecoin.conf ]] ; then
     echo "Renaming configuration file spectrecoin.conf to aliaswallet.conf"
     mv ~/.aliaswallet/spectrecoin.conf ~/.aliaswallet/aliaswallet.conf
     echo "    Done"
 fi
 echo ""
+
+# ----------------------------------------------------------------------------
+# Remove spectrecoin binaries
+if [[ -e ${installPath}/spectrecoind ]] ; then
+    echo "Determining spectrecoind binary version"
+    # Version is something like "v2.2.2.0 (86e9b92 - 2019-01-26 17:20:20 +0100)"
+    # but only the version and the commit hash separated by "_" is used later on.
+    # Option '-version' is working since v3.x
+    queryResult=$(${installPath}/spectrecoind -version)
+    currentVersion=$(echo ${queryResult/\(/} | cut -d ' ' -f 1)
+    gitHash=$(echo ${queryResult/\(/} | cut -d ' ' -f 2)
+    if [[ -n "${gitHash}" ]] ; then
+        fullVersion=${currentVersion}-${gitHash}
+    else
+        fullVersion=${currentVersion}
+    fi
+    if [[ -z "${fullVersion}" ]] ; then
+        fullVersion=$(date +%Y%m%d-%H%M)
+        echo "    Unable to determine version of current binaries, using timestamp '${fullVersion}'"
+    else
+        echo "    Creating backup of current version ${fullVersion}"
+    fi
+    if [[ -f ${installPath}/spectrecoind-${fullVersion} ]] ; then
+        echo "    Backup of current version already existing"
+    else
+        sudo mv ${installPath}/spectrecoind ${installPath}/spectrecoind-${fullVersion}
+        if [[ -e ${installPath}/spectrecoin ]] ; then
+            sudo mv ${installPath}/spectrecoin  ${installPath}/spectrecoin-${fullVersion}
+        fi
+        echo "    Done"
+    fi
+else
+    echo "Binary ${installPath}/spectrecoind not found, skip backup creation"
+fi
+echo ""
+
+# ----------------------------------------------------------------------------
+# Rename and update service
+sudo systemctl disable spectrecoind
+sudo mv /lib/systemd/system/spectrecoind.service /lib/systemd/system/aliaswalletd.service
+sudo sed -i \
+    -e "s/Spectrecoin/Aliaswallet/g" \
+    -e "s/spectrecoind/aliaswalletd/g" \
+    /lib/systemd/system/aliaswalletd.service
+sudo systemctl daemon-reload
+sudo systemctl enable aliaswalletd
+
+# ----------------------------------------------------------------------------
+# Rename Shell-UI directory
+if [[ -d ~/spectrecoin-sh-rpc-ui ]] ; then
+    mv ~/spectrecoin-sh-rpc-ui ~/aliaswallet-sh-rpc-ui
+    cd ~/aliaswallet-sh-rpc-ui
+    git remote set-url origin https://github.com/aliascash/aliaswallet-sh-rpc-ui.git
+    git pull
+    cd - >/dev/null
+fi
+
+# ----------------------------------------------------------------------------
+# Update alias definitions
+sed -i \
+    -e "s/spectrecoin/aliaswallet/g" \
+    ~/.bash_aliases
 
 # ----------------------------------------------------------------------------
 # Use ca-certificates if available
